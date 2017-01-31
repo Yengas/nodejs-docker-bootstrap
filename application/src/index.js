cb05cdc24d66078f02e7532b15f82f3a8a1054ac
Yengas <yigitcan@hotmail.com.tr>
2017-01-31 19:58:37 +0300

    Add rating endpoint to rate a song.

--- application/src/index.js
+++ application/src/index.js
@@ -5,15 +5,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authorController = require('./routes/author');
 const songController = require('./routes/song');
+const ratingController = require('./routes/rating');
 const app = express();
 
 app.use(bodyParser.json({ extended: true }));
 
 app.get('/healtz', require('./routes/healthz'));
+
 app.get('/author/get', authorController.list);
 app.get('/author/:id/songs', authorController.songs);
+
 app.get('/song/get', songController.list);
 app.post('/song/search', songController.search);
+app.post('/song/:id/rate', ratingController.rate);
 
 pool
   .query('SELECT 1')
---
+++ application/src/routes/rating.js
@@ -0,0 +1,25 @@
+const pool = require('../database');
+const log = require('../logger');
+
+// Make sure the given parameters are integers
+// and the rate is between 1 and 10 inclusive.
+function validateRating(song, rate){
+  if(!song || !rate) return false;
+  rate = parseInt(rate);
+  return  rate && parseInt(song) && rate > 0 && rate <= 10;
+}
+
+// An endpoint to rate a given song.
+module.exports.rate = function(req, res){
+  if(!validateRating(req.params.id, req.body.rating))
+    return res.json({ error: true, reason: 'Invalid rating!' });
+
+  // Will fail if the song doesn't exists because of the foreign key constraint.
+  pool
+    .query(`INSERT INTO ratings (song_id, rating) VALUES (?, ?)`, [req.params.id, req.body.rating])
+    .then(() => res.json({ succeses: true, message: "Ok!" }))
+    .catch(err => {
+      log.error({ err, params: { song: req.params.id, rating: req.body.rating }}, "Couldn't rate a song.");
+      res.json({ error: true, reason: err.message });
+    });
+};
